b2299122a3f83df20e00ace4d2d08dbf330dad63
Xiao Jin <debugeek@gmail.com>
2021-02-10 10:29:28 +0800

    Remove unused code

--- XMRMiner/XMRJob.swift
+++ XMRMiner/XMRJob.swift
@@ -8,34 +8,6 @@
 
 import Foundation
 
-struct XMRHash {
-    static let size = 32
-
-    fileprivate var raw = [UInt8]()
-
-    var bytes: [UInt8] {
-        get { return raw }
-        set { raw = newValue }
-    }
-    init(bytes: [UInt8]) { self.bytes = bytes }
-
-    var uints: [UInt64] {
-        get {
-            let length = Int(ceil(Double(raw.count)/Double(MemoryLayout<UInt64>.size)))
-            var buffer = [UInt64](repeating: 0, count: length)
-            memmove(&buffer, raw, length*MemoryLayout<UInt64>.size)
-            return buffer
-        }
-        set {
-            let length = newValue.count*MemoryLayout<UInt64>.size
-            var buffer = [UInt8](repeating: 0, count: length)
-            memmove(&buffer, newValue, length)
-            raw = buffer
-        }
-    }
-    init(uints: [UInt64]) { self.uints = uints }
-}
-
 struct XMRJob {
 
     let jobId: String
